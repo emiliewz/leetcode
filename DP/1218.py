ae9a15e51e5ac5e6978dcda43a0da92a727e7bf7
emilie <loveskyship@gmail.com>
2024-01-07 22:23:24 -0800

    1218. Longest Arithmetic Subsequence of Given Difference

--- DP/1218.py
+++ DP/1218.py
@@ -10,3 +10,15 @@ class Solution:
             dp[n] = 1 + dp.get(n - difference, 0)
 
         return max(dp.values())
+
+
+class Solution:
+    def longestSubsequence(self, arr: List[int], difference: int) -> int:
+        dp = {}
+
+        for i in arr:
+            if i - difference in dp:
+                dp[i] = dp[i - difference] + 1
+            else:
+                dp[i] = 1
+        return max(dp.values())
