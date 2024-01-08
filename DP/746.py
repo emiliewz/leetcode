bef71beb5d65ed7c9022667b7c772e4aabb0a34a
emilie <loveskyship@gmail.com>
2024-01-07 22:02:52 -0800

    746. Min Cost Climbing Stairs

--- DP/746.py
+++ DP/746.py
@@ -4,6 +4,16 @@ from typing import List
 
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
+        f, s = cost[-1], 0
+
+        for i in range(len(cost) - 2, -1, -1):
+            f, s = min(f, s) + cost[i], f
+
+        return min(f, s)
+
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
         for i in range(len(cost) - 3, -1, -1):
             cost[i] += min(cost[i + 1], cost[i + 2])
 
