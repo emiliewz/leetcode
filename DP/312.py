d511b7ae025dbeb997e80db2d74f95853d0f1192
emilie <loveskyship@gmail.com>
2024-01-09 22:19:57 -0800

    312. Burst Balloons

--- DP/312.py
+++ DP/312.py
@@ -5,6 +5,24 @@ from typing import List
 
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
+        @cache
+        def dp(l, r):
+            if l > r:
+                return 0
+
+            max_coins = 0
+            for i in range(l, r + 1):
+                coins = nums[l - 1] * nums[r + 1] * nums[i]
+                max_coins = max(max_coins, coins + dp(l, i - 1) + dp(i + 1, r))
+
+            return max_coins
+
+        nums = [1] + nums + [1]
+        return dp(1, len(nums) - 2)
+
+
+class Solution:
+    def maxCoins(self, nums: List[int]) -> int:
         nums = [1] + nums + [1]
 
         @cache
