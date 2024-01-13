11cda4aaeb649a75cbc56da26e548392b5210649
emilie <loveskyship@gmail.com>
2024-01-12 19:16:34 -0800

    213. House Robber II

--- DP/213.py
+++ DP/213.py
@@ -4,6 +4,23 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
+        if n <= 3:
+            return max(nums)
+
+        def house_rob(nums):
+            f, s = nums[-1], 0
+            for i in range(len(nums) - 2, -1, -1):
+                f, s = max(nums[i] + s, f), f
+            return f
+
+        return max(house_rob(nums[1:]), house_rob(nums[:-1]))
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
         if len(nums) == 1:
             return nums[0]
         if len(nums) == 2:
