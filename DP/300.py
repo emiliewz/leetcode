8c552979ef412700c95c71ea122d0676f56447ac
emilie <loveskyship@gmail.com>
2024-01-12 22:03:49 -0800

    300. Longest Increasing Subsequence

--- DP/300.py
+++ DP/300.py
@@ -1,5 +1,36 @@
 # 300. Longest Increasing Subsequence
+from bisect import bisect_left
 from typing import List
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if len(nums) == 1:
+            return 1
+        a = []
+
+        for n in nums:
+            if not a or n > a[-1]:
+                a.append(n)
+            else:
+                pos = bisect_left(a, n)
+                a[pos] = n
+        return len(a)
+
+
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return 1
+        a = [1] * n
+
+        for i in range(1, n):
+            for j in range(i):
+                if nums[i] > nums[j]:
+                    a[i] = max(a[i], a[j] + 1)
+
+        return max(a)
 
 
 class Solution:
