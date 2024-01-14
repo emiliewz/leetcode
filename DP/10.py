9f3a0837485b7405ea4b9f2562c3470d9457675a
emilie <loveskyship@gmail.com>
2024-01-13 18:19:13 -0800

    10. Regular Expression Matching

--- DP/10.py
+++ DP/10.py
@@ -1,4 +1,50 @@
 # 10. Regular Expression Matching
+from functools import cache
+
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+        a = [[False] * (n + 1) for _ in range(m + 1)]
+        a[0][0] = True
+        for j in range(2, n + 1):
+            if p[j - 1] == "*":
+                a[0][j] = a[0][j - 2]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if p[j - 1] == "." or s[i - 1] == p[j - 1]:
+                    a[i][j] = a[i - 1][j - 1]
+                elif p[j - 1] == "*":
+                    a[i][j] = a[i][j - 2] or (
+                        p[j - 2] == "." or s[i - 1] == p[j - 2] and a[i - 1][j]
+                    )
+
+        return a[-1][-1]
+
+
+class Solution:
+    def isMatch(self, s: str, p: str) -> bool:
+        m, n = len(s), len(p)
+
+        @cache
+        def dp(i, j):
+            if j == n:
+                return i == m
+            if i == m:
+                return j + 1 < n and p[j + 1] == "*" and dp(i, j + 2)
+
+            match = p[j] == "." or p[j] == s[i]
+
+            if j + 1 < n and p[j + 1] == "*":
+                return dp(i, j + 2) or (match and dp(i + 1, j))
+            if match:
+                return dp(i + 1, j + 1)
+            return False
+
+        return dp(0, 0)
+
+
 class Solution:
     def isMatch(self, s: str, p: str) -> bool:
         m, n = len(s), len(p)
