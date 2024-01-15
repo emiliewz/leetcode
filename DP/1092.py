056dbf1c7dc6bf3a9e42a0307d6d859203db21cb
emilie <loveskyship@gmail.com>
2024-01-14 23:40:30 -0800

    1092. Shortest Common Supersequence

--- DP/1092.py
+++ DP/1092.py
@@ -2,6 +2,107 @@
 class Solution:
     def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
         m, n = len(str1), len(str2)
+        a = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if str1[i - 1] == str2[j - 1]:
+                    a[i][j] = a[i - 1][j - 1] + 1
+                else:
+                    a[i][j] = max(a[i - 1][j], a[i][j - 1])
+        res = ""
+        i, j = m, n
+        while i > 0 and j > 0:
+            if str1[i - 1] == str2[j - 1]:
+                res += str1[i - 1]
+                i -= 1
+                j -= 1
+            else:
+                if a[i - 1][j] > a[i][j - 1]:
+                    res += str1[i - 1]
+                    i -= 1
+                else:
+                    res += str2[j - 1]
+                    j -= 1
+        while i > 0:
+            res += str1[i - 1]
+            i -= 1
+        while j > 0:
+            res += str2[j - 1]
+            j -= 1
+        return res[::-1]
+
+
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        m, n = len(str1), len(str2)
+        a = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if str1[i - 1] == str2[j - 1]:
+                    a[i][j] = a[i - 1][j - 1] + 1
+                else:
+                    a[i][j] = max(a[i - 1][j], a[i][j - 1])
+
+        lcs = ""
+        i, j = m, n
+        while i > 0 and j > 0:
+            if str1[i - 1] == str2[j - 1]:
+                lcs += str1[i - 1]
+                i -= 1
+                j -= 1
+            else:
+                if a[i - 1][j] > a[i][j - 1]:
+                    i -= 1
+                else:
+                    j -= 1
+        res = ""
+        i, j = 0, 0
+        for c in lcs[::-1]:
+            while str1[i] != c:
+                res += str1[i]
+                i += 1
+            while str2[j] != c:
+                res += str2[j]
+                j += 1
+            res += c
+            i += 1
+            j += 1
+        return res + str1[i:] + str2[j:]
+
+
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        m, n = len(str1), len(str2)
+        a = [""] * (n + 1)
+
+        for i in range(1, m + 1):
+            tmp = a[:]
+            for j in range(1, n + 1):
+                if str1[i - 1] == str2[j - 1]:
+                    a[j] = tmp[j - 1] + str1[i - 1]
+                else:
+                    a[j] = max(tmp[j], a[j - 1], key=len)
+
+        res = ""
+        i, j = 0, 0
+        for c in a[-1]:
+            while str1[i] != c:
+                res += str1[i]
+                i += 1
+            while str2[j] != c:
+                res += str2[j]
+                j += 1
+            res += c
+            i += 1
+            j += 1
+        return res + str1[i:] + str2[j:]
+
+
+class Solution:
+    def shortestCommonSupersequence(self, str1: str, str2: str) -> str:
+        m, n = len(str1), len(str2)
 
         def get_lcs():
             dp = [[0] * (n + 1) for _ in range(m + 1)]
