3d5f79dfb7efb89530d09fc4976135010e0fad5b
emilie <loveskyship@gmail.com>
2024-01-14 21:35:05 -0800

    583. Delete Operation for Two Strings

--- DP/583.py
+++ DP/583.py
@@ -1,4 +1,56 @@
 # 583. Delete Operation for Two Strings
+from functools import cache
+
+
+class Solution:
+    def minDistance(self, A: str, B: str) -> int:
+        m, n = len(A), len(B)
+
+        a = [0] * (n + 1)
+        for i in range(1, m + 1):
+            tmp = a[:]
+            for j in range(1, n + 1):
+                if A[i - 1] == B[j - 1]:
+                    a[j] = tmp[j - 1] + 1
+                else:
+                    a[j] = max(a[j - 1], tmp[j])
+        return m + n - 2 * a[-1]
+
+
+class Solution:
+    def minDistance(self, A: str, B: str) -> int:
+        m, n = len(A), len(B)
+        a = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if A[i - 1] == B[j - 1]:
+                    a[i][j] = 1 + a[i - 1][j - 1]
+                else:
+                    a[i][j] = max(a[i][j - 1], a[i - 1][j])
+
+        return m + n - 2 * a[-1][-1]
+
+
+class Solution:
+    def minDistance(self, A: str, B: str) -> int:
+        m, n = len(A), len(B)
+
+        @cache
+        def dp(i, j):
+            if i == m:
+                return n - j
+            if j == n:
+                return m - i
+
+            if A[i] == B[j]:
+                return dp(i + 1, j + 1)
+            else:
+                return min(dp(i, j + 1), dp(i + 1, j)) + 1
+
+        return dp(0, 0)
+
+
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
         m, n = len(word1), len(word2)
