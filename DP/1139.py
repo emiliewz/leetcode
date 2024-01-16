f0957b1cc14b2eb00b3048d85be50907a8987676
emilie <loveskyship@gmail.com>
2024-01-15 19:44:45 -0800

    1139. Largest 1-Bordered Square

--- DP/1139.py
+++ DP/1139.py
@@ -6,6 +6,25 @@ class Solution:
     def largest1BorderedSquare(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
         max_len = 0
+        a = [[(0, 0)] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if grid[i - 1][j - 1] == 1:
+                    a[i][j] = (a[i][j - 1][0] + 1, a[i - 1][j][1] + 1)
+                    if min(a[i][j]) > max_len:
+                        for k in range(min(a[i][j]), max_len, -1):
+                            if a[i][j - k + 1][1] >= k and a[i - k + 1][j][0] >= k:
+                                max_len = k
+                                break
+
+        return max_len**2
+
+
+class Solution:
+    def largest1BorderedSquare(self, grid: List[List[int]]) -> int:
+        m, n = len(grid), len(grid[0])
+        max_len = 0
 
         # (x, y) in dp, refer to the accumulative width, height
         dp = [[(0, 0)] * (n + 1) for _ in range(m + 1)]
