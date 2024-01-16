fc226cd3f192bdd336c6c64cea9ae016815be1d6
emilie <loveskyship@gmail.com>
2024-01-15 21:53:35 -0800

    494. Target Sum

--- DP/494.py
+++ DP/494.py
@@ -5,6 +5,33 @@ from typing import List
 
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        sums = sum(nums)
+        if sums < abs(target) or (sums + target) & 1:
+            return 0
+        new_target = (target + sums) >> 1
+        a = [0] * (new_target + 1)
+        a[0] = 1
+
+        for n in nums:
+            for i in range(new_target, n - 1, -1):
+                a[i] += a[i - n]
+
+        return a[new_target]
+
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        @cache
+        def dp(i, cur):
+            if i == len(nums):
+                return int(cur == target)
+            return dp(i + 1, cur - nums[i]) + dp(i + 1, cur + nums[i])
+
+        return dp(0, 0)
+
+
+class Solution:
+    def findTargetSumWays(self, nums: List[int], target: int) -> int:
         memo = {}
 
         def dp(i, k):
