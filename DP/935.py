01da0988591736a1d319716fbe5ea4e6c666ec24
emilie <loveskyship@gmail.com>
2024-01-15 20:32:03 -0800

    935. Knight Dialer

--- DP/935.py
+++ DP/935.py
@@ -4,6 +4,58 @@ from functools import cache
 
 class Solution:
     def knightDialer(self, n: int) -> int:
+        MOD = 10**9 + 7
+        neighbors = {
+            0: [4, 6],
+            1: [6, 8],
+            2: [7, 9],
+            3: [4, 8],
+            4: [0, 3, 9],
+            5: [],
+            6: [0, 1, 7],
+            7: [2, 6],
+            8: [1, 3],
+            9: [2, 4],
+        }
+
+        a = [1] * 10
+
+        for _ in range(n - 1):
+            tmp = [0] * 10
+            for i in range(10):
+                for k in neighbors[i]:
+                    tmp[k] = (tmp[k] + a[i]) % MOD
+            a = tmp
+        return sum(a) % MOD
+
+
+class Solution:
+    def knightDialer(self, n: int) -> int:
+        directions = [(1, 2), (1, -2), (2, 1), (2, -1)]
+        MOD = 10**9 + 7
+
+        @cache
+        def dp(x, y, k):
+            if x < 0 or x >= 4 or y < 0 or y >= 3 or (x == 3 and y != 1):
+                return 0
+            if k == 0:
+                return 1
+
+            res = 0
+            for dx, dy in directions:
+                res += dp(x + dx, y + dy, k - 1)
+                res += dp(x - dx, y - dy, k - 1)
+            return res % MOD
+
+        res = 0
+        for i in range(4):
+            for j in range(3):
+                res += dp(i, j, n - 1)
+        return res % MOD
+
+
+class Solution:
+    def knightDialer(self, n: int) -> int:
         directions = [
             (-1, -2),
             (-1, 2),
