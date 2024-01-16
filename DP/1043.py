5fc32443d616d1468aff6466923b8bdc1433224d
emilie <loveskyship@gmail.com>
2024-01-15 22:12:22 -0800

    1043. Partition Array for Maximum Sum

--- DP/1043.py
+++ DP/1043.py
@@ -5,6 +5,21 @@ from typing import List
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
         n = len(arr)
+        if n <= k:
+            return max(arr) * n
+        a = [0] * (n + 1)
+
+        for i in range(n + 1):
+            cur_max = 0
+            for j in range(1, min(i, k) + 1):
+                cur_max = max(arr[i - j], cur_max)
+                a[i] = max(a[i], a[i - j] + cur_max * j)
+        return a[-1]
+
+
+class Solution:
+    def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        n = len(arr)
         dp = [0] * (n + 1)
 
         for i in range(1, n + 1):
