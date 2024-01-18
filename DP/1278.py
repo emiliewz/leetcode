3d8ae30e1705d2706c0fad82d5203001918f7f42
emilie <loveskyship@gmail.com>
2024-01-17 23:35:52 -0800

    1278. Palindrome Partitioning III

--- DP/1278.py
+++ DP/1278.py
@@ -4,6 +4,64 @@ from functools import cache
 
 class Solution:
     def palindromePartition(self, s: str, k: int) -> int:
+        n = len(s)
+
+        @cache
+        def cost(i, j):
+            if i >= j:
+                return 0
+            return (s[i] != s[j - 1]) + cost(i + 1, j - 1)
+
+        @cache
+        def dp(i, k):
+            if k == n - i:
+                return 0
+            if k == 1:
+                return cost(i, n)
+
+            costs = float("inf")
+            for j in range(i + 1, n - k + 2):
+                costs = min(costs, cost(i, j) + dp(j, k - 1))
+            return costs
+
+        return dp(0, k)
+
+
+class Solution:
+    def palindromePartition(self, s: str, k: int) -> int:
+        n = len(s)
+
+        @cache
+        def cost(w):
+            length = len(w)
+            if length == 1:
+                return 0
+            l, r = 0, length - 1
+            cost = 0
+            while l < r:
+                if w[l] != w[r]:
+                    cost += 1
+                l += 1
+                r -= 1
+            return cost
+
+        @cache
+        def dp(i, k):
+            if k == 0 and i == n:
+                return 0
+            if k == 0 or i == n:
+                return float("inf")
+
+            costs = float("inf")
+            for j in range(i + 1, n + 1):
+                costs = min(costs, cost(s[i:j]) + dp(j, k - 1))
+            return costs
+
+        return dp(0, k)
+
+
+class Solution:
+    def palindromePartition(self, s: str, k: int) -> int:
         @cache
         def cost(i, j=len(s) - 1):
             return 0 if i >= j else cost(i + 1, j - 1) + (s[i] != s[j])
