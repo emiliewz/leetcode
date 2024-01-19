eac4ab06303882456bb9e75ceee0e41c25f83b1a
emilie <loveskyship@gmail.com>
2024-01-18 19:15:01 -0800

    410. Split Array Largest Sum

--- DP/410.py
+++ DP/410.py
@@ -7,6 +7,52 @@ class Solution:
     def splitArray(self, nums: List[int], k: int) -> int:
         l, r = max(nums), sum(nums)
 
+        def can_split(target):
+            count, total = 1, 0
+            for n in nums:
+                count += total + n > target
+                total = total + n if total + n <= target else n
+            return count <= k
+
+        while l <= r:
+            mid = l + (r - l) // 2
+            if can_split(mid):
+                r = mid - 1
+            else:
+                l = mid + 1
+
+        return l
+
+
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        n = len(nums)
+        if k == n:
+            return max(nums)
+        sums = [0] * (n + 1)
+        for i in range(1, n + 1):
+            sums[i] = sums[i - 1] + nums[i - 1]
+
+        @cache
+        def dp(i, k):
+            if k == 1:
+                return sums[n] - sums[i]
+            if k == n - i:
+                return max(nums[i:])
+
+            min_sum = float("inf")
+            for j in range(i + 1, n + 1):
+                cur_max = max(sums[j] - sums[i], dp(j, k - 1))
+                min_sum = min(min_sum, cur_max)
+            return min_sum
+
+        return dp(0, k)
+
+
+class Solution:
+    def splitArray(self, nums: List[int], k: int) -> int:
+        l, r = max(nums), sum(nums)
+
         def canSplit(max_sum):
             cur_sum, count = 0, 0
             for n in nums:
