6e1669f4349d4260efb29553ba3a71b1ad69b7e3
emilie <loveskyship@gmail.com>
2024-01-19 09:52:58 -0800

    1039. Minimum Score Triangulation of Polygon

--- DP/1039.py
+++ DP/1039.py
@@ -5,6 +5,23 @@ from typing import List
 
 class Solution:
     def minScoreTriangulation(self, values: List[int]) -> int:
+        n = len(values)
+        a = [[0] * n for _ in range(n)]
+
+        for l in range(3, n + 1):
+            for i in range(n - l + 1):
+                j = i + l - 1
+                a[i][j] = float("inf")
+                for k in range(i + 1, j):
+                    a[i][j] = min(
+                        a[i][j], a[i][k] + a[k][j] + values[i] * values[j] * values[k]
+                    )
+
+        return a[0][-1]
+
+
+class Solution:
+    def minScoreTriangulation(self, values: List[int]) -> int:
         @cache
         def dp(i, j):
             res = float("inf")
