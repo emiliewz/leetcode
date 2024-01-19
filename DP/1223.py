06e512cac353c843265ea32bdafde7b501c31782
emilie <loveskyship@gmail.com>
2024-01-18 20:40:53 -0800

    1223. Dice Roll Simulation

--- DP/1223.py
+++ DP/1223.py
@@ -8,6 +8,26 @@ class Solution:
         MOD = 10**9 + 7
 
         @cache
+        def dp(cur, j, k):
+            if k == n:
+                return 1
+
+            res = 0
+            for i in range(6):
+                if i != cur:
+                    res += dp(i, 1, k + 1)
+                elif j < rollMax[i]:
+                    res += dp(i, j + 1, k + 1)
+            return res
+
+        return dp(-1, 0, 0) % MOD
+
+
+class Solution:
+    def dieSimulator(self, n: int, rollMax: List[int]) -> int:
+        MOD = 10**9 + 7
+
+        @cache
         def dp(rolls, j, k):
             if rolls == n:
                 return 1
