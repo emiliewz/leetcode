5d6d58fdd0d8b3f6c6c3e89095ec32ca96db035d
emilie <loveskyship@gmail.com>
2024-01-19 22:04:10 -0800

    79. Word Search

--- Search/79.py
+++ Search/79.py
@@ -4,6 +4,36 @@ from typing import List
 
 class Solution:
     def exist(self, board: List[List[str]], word: str) -> bool:
+        m, n = len(board), len(board[0])
+        l = len(word)
+
+        def dfs(i, j, k):
+            if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != word[k]:
+                return False
+            if k == l - 1:
+                return True
+
+            board[i][j] = 0
+            res = (
+                dfs(i + 1, j, k + 1)
+                or dfs(i - 1, j, k + 1)
+                or dfs(i, j + 1, k + 1)
+                or dfs(i, j - 1, k + 1)
+            )
+            board[i][j] = word[k]
+            return res
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] == word[0]:
+                    if dfs(i, j, 0):
+                        return True
+
+        return False
+
+
+class Solution:
+    def exist(self, board: List[List[str]], word: str) -> bool:
         r = len(board)
         c = len(board[0])
 
