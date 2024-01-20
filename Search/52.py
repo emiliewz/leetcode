c85c7e6c06c21193345c5feb1ea326648a25ffd5
emilie <loveskyship@gmail.com>
2024-01-20 13:27:59 -0800

    52. N-Queens II

--- Search/52.py
+++ Search/52.py
@@ -2,6 +2,77 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
         res = 0
+
+        def dfs(queens, summ, diff):
+            nonlocal res
+            row = len(queens)
+            if row == n:
+                res += 1
+                return
+
+            for col in range(n):
+                if col in queens or row + col in summ or row - col in diff:
+                    continue
+                dfs(queens + [col], summ + [row + col], diff + [row - col])
+
+        dfs([], [], [])
+        return res
+
+
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        res = 0
+        queens, summ, diff = set(), set(), set()
+
+        def dfs(row):
+            nonlocal res
+            if row == n:
+                res += 1
+                return
+
+            for col in range(n):
+                if col in queens or row + col in summ or row - col in diff:
+                    continue
+                summ.add(row + col)
+                diff.add(row - col)
+                queens.add(col)
+                dfs(row + 1)
+                summ.remove(row + col)
+                diff.remove(row - col)
+                queens.remove(col)
+
+        dfs(0)
+        return res
+
+
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        res = 0
+        summ, diff = set(), set()
+
+        def dfs(queens):
+            nonlocal res
+            row = len(queens)
+            if row == n:
+                res += 1
+                return
+
+            for col in range(n):
+                if col in queens or row + col in summ or row - col in diff:
+                    continue
+                summ.add(row + col)
+                diff.add(row - col)
+                dfs(queens + [col])
+                summ.remove(row + col)
+                diff.remove(row - col)
+
+        dfs([])
+        return res
+
+
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        res = 0
         negDiag = set()
         posDiag = set()
         col = set()
