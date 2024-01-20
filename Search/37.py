6e89d2a2cdcf11a66c6378742f0d5166622e4f3c
emilie <loveskyship@gmail.com>
2024-01-20 12:45:27 -0800

    37. Sudoku Solver

--- Search/37.py
+++ Search/37.py
@@ -7,6 +7,51 @@ class Solution:
         """
         Do not return anything, modify board in-place instead.
         """
+        rows = [[0] * 10 for _ in range(9)]
+        cols = [[0] * 10 for _ in range(9)]
+        boxes = [[0] * 10 for _ in range(9)]
+        k = 0
+
+        for i in range(9):
+            for j in range(9):
+                num = board[i][j]
+                if num != ".":
+                    rows[i][int(num)] = 1
+                    cols[j][int(num)] = 1
+                    boxes[3 * (i // 3) + j // 3][int(num)] = 1
+                else:
+                    k += 1
+
+        def dfs(i, j):
+            if i == 9:
+                return True
+            if j == 9:
+                return dfs(i + 1, 0)
+            if board[i][j] != ".":
+                return dfs(i, j + 1)
+            for k in range(1, 10):
+                if rows[i][k] or cols[j][k] or boxes[3 * (i // 3) + j // 3][k]:
+                    continue
+                board[i][j] = str(k)
+                rows[i][k] = 1
+                cols[j][k] = 1
+                boxes[3 * (i // 3) + j // 3][k] = 1
+                if dfs(i, j + 1):
+                    return True
+                board[i][j] = "."
+                rows[i][k] = 0
+                cols[j][k] = 0
+                boxes[3 * (i // 3) + j // 3][k] = 0
+
+        dfs(0, 0)
+        return board
+
+
+class Solution:
+    def solveSudoku(self, board: List[List[str]]) -> None:
+        """
+        Do not return anything, modify board in-place instead.
+        """
         rows, cols, blocks = (
             [[0] * 10 for _ in range(9)],
             [[0] * 10 for _ in range(9)],
