67b76387353f6ddf2fe589f4af121e1b61b6999f
emilie <loveskyship@gmail.com>
2024-01-20 13:20:08 -0800

    51. N-Queens

--- Search/51.py
+++ Search/51.py
@@ -4,6 +4,47 @@ from typing import List
 
 class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
+        res = []
+        summ = set()
+        diff = set()
+
+        def dfs(q):
+            row = len(q)
+            if row == n:
+                res.append(q)
+                return
+            for col in range(n):
+                if col in q or row + col in summ or row - col in diff:
+                    continue
+                diff.add(row - col)
+                summ.add(row + col)
+                dfs(q + [col])
+                diff.remove(row - col)
+                summ.remove(row + col)
+
+        dfs([])
+        return [["." * i + "Q" + "." * (n - i - 1) for i in queens] for queens in res]
+
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
+        def dfs(queens, diff, summ):
+            row = len(queens)
+            if row == n:
+                res.append(["." * i + "Q" + "." * (n - i - 1) for i in queens])
+                return
+            for col in range(n):
+                if col in queens or row + col in summ or row - col in diff:
+                    continue
+                dfs(queens + [col], diff + [row - col], summ + [row + col])
+
+        res = []
+        dfs([], [], [])
+        return res
+
+
+class Solution:
+    def solveNQueens(self, n: int) -> List[List[str]]:
         posDiag = set()
         negDiag = set()
         col = set()
