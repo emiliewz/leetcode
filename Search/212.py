fb192f049db4f74b94b13420db04b41ab418d188
emilie <loveskyship@gmail.com>
2024-01-20 14:32:00 -0800

    212. Word Search II

--- Search/212.py
+++ Search/212.py
@@ -2,6 +2,86 @@
 from typing import List
 
 
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        def dfs(i, j, root):
+            if i < 0 or i >= m or j < 0 or j >= n or board[i][j] not in root:
+                return
+            letter = board[i][j]
+            cur = root[letter]
+            # word = cur.pop('#', False)
+            if "#" in cur:
+                res.append(cur.pop("#"))
+            # if word:
+            #     res.append(word)
+
+            board[i][j] = 0
+            dfs(i + 1, j, cur)
+            dfs(i - 1, j, cur)
+            dfs(i, j + 1, cur)
+            dfs(i, j - 1, cur)
+            board[i][j] = letter
+            if not cur:
+                root.pop(letter)
+            return res
+
+        m, n = len(board), len(board[0])
+        res = []
+        a = {}
+
+        for word in words:
+            cur = a
+            for letter in word:
+                cur = cur.setdefault(letter, {})
+            cur["#"] = word
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] in a:
+                    dfs(i, j, a)
+
+        return res
+
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        def dfs(i, j, k, word):
+            if i < 0 or i >= m or j < 0 or j >= n or board[i][j] != word[k]:
+                return False
+            if k == len(word) - 1:
+                return True
+            board[i][j] = 0
+            res = (
+                dfs(i + 1, j, k + 1, word)
+                or dfs(i - 1, j, k + 1, word)
+                or dfs(i, j + 1, k + 1, word)
+                or dfs(i, j - 1, k + 1, word)
+            )
+            board[i][j] = word[k]
+            return res
+
+        m, n = len(board), len(board[0])
+        res = []
+        a = {}
+
+        for word in words:
+            a.setdefault(word[0], []).append(word)
+
+        for i in range(m):
+            for j in range(n):
+                if board[i][j] in a:
+                    all_words = a[board[i][j]]
+                    for word in all_words[:]:
+                        if dfs(i, j, 0, word):
+                            res.append(word)
+                            if len(all_words) > 1:
+                                a[board[i][j]].remove(word)
+                            else:
+                                a.pop(board[i][j])
+
+        return res
+
+
 class TrieNode:
     def __init__(self):
         self.children = {}
