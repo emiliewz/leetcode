8caff2a7abe9fdd3b54f7eda345777228b533eb4
emilie <loveskyship@gmail.com>
2024-01-19 23:38:18 -0800

    943. Find the Shortest Superstring

--- Search/943.py
+++ Search/943.py
@@ -1,6 +1,36 @@
 # 943. Find the Shortest Superstring
 
+from functools import cache
+from itertools import permutations
 from typing import List
+
+
+class Solution:
+    def shortestSuperstring(self, words: List[str]) -> str:
+        @cache
+        def connect(f, s):
+            m = min(len(f), len(s))
+            l, i = 0, 1
+
+            while i <= m:
+                if f[-i:] == s[:i]:
+                    l = i
+                i += 1
+            return s[l:]
+
+        n = len(words)
+        a = [[(float("inf"), "")] * n for _ in range(1 << n)]
+
+        for i in range(n):
+            a[1 << i][i] = (len(words[i]), words[i])
+
+        for i in range(1 << n):
+            word_bits = [j for j in range(n) if i & 1 << j]
+            for f, s in permutations(word_bits, 2):
+                new_word = a[i ^ 1 << s][f][1] + connect(words[f], words[s])
+                a[i][s] = min(a[i][s], (len(new_word), new_word))
+
+        return min(a[-1])[1]
 
 
 class Solution:
