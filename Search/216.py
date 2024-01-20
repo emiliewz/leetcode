5df1f4edf02ef9332d3b4eea2500184b4835cc61
emilie <loveskyship@gmail.com>
2024-01-19 22:14:21 -0800

    216. Combination Sum III

--- Search/216.py
+++ Search/216.py
@@ -6,6 +6,25 @@ class Solution:
     def combinationSum3(self, k: int, n: int) -> List[List[int]]:
         res = []
 
+        def dfs(i, cur, total):
+            if len(cur) > k or total > n:
+                return
+            if len(cur) == k:
+                if total == n:
+                    res.append(cur)
+                return
+
+            for j in range(i, 10):
+                dfs(j + 1, cur + [j], total + j)
+
+        dfs(1, [], 0)
+        return res
+
+
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        res = []
+
         def backtrack(i, cur, total):
             if len(cur) == k:
                 if total == n:
