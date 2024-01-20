45afd4f6cbfce1198e2f2d1fbd76a68fa04d0160
emilie <loveskyship@gmail.com>
2024-01-19 21:30:15 -0800

    78. Subsets

--- Search/78.py
+++ Search/78.py
@@ -4,12 +4,23 @@ from typing import List
 
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
+        def dfs(i, cur):
+            res.append(cur)
+            for j in range(i, n):
+                dfs(j + 1, cur + [nums[j]])
+
+        res = []
+        n = len(nums)
+        dfs(0, [])
+        return res
+
+
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
         res = []
 
         def dfs(pos, cur):
             res.append(cur)
-            if len(cur) == len(nums):
-                return
 
             for i in range(pos, len(nums)):
                 dfs(i + 1, cur + [nums[i]])
