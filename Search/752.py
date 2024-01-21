43564c94a7309bd33830de732923c8b84817e4bc
emilie <loveskyship@gmail.com>
2024-01-20 20:13:48 -0800

    752. Open the Lock

--- Search/752.py
+++ Search/752.py
@@ -1,5 +1,40 @@
 # 752. Open the Lock
-from typing import Deque, List
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def openLock(self, deadends: List[str], target: str) -> int:
+        if "0000" in deadends:
+            return -1
+
+        def get_neighbours(lock):
+            res = []
+            for i in range(4):
+                res.append(lock[:i] + str((int(lock[i]) + 1 + 10) % 10) + lock[i + 1 :])
+                res.append(lock[:i] + str((int(lock[i]) - 1 + 10) % 10) + lock[i + 1 :])
+            return res
+
+        q = deque()
+        q.append("0000")
+        visit = set(deadends)
+        visit.add("0000")
+        steps = 0
+
+        while q:
+            for _ in range(len(q)):
+                cur = q.popleft()
+                if cur == target:
+                    return steps
+
+                for c in get_neighbours(cur):
+                    if c not in visit:
+                        visit.add(c)
+                        q.append(c)
+
+            steps += 1
+
+        return -1
 
 
 class Solution:
@@ -15,7 +50,7 @@ class Solution:
                     res.append(cur)
             return res
 
-        q = Deque()
+        q = deque()
         q.append(["0000", 0])
         visit = set(deadends)
 
