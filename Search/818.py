b74ce420e1b02dae7ce9a20c4b4076c44029c919
emilie <loveskyship@gmail.com>
2024-01-20 20:19:23 -0800

    818. Race Car

--- Search/818.py
+++ Search/818.py
@@ -1,12 +1,32 @@
 # 818. Race Car
-from typing import Deque
+from collections import deque
+
+
+class Solution:
+    def racecar(self, target: int) -> int:
+        q = deque()
+        q.append((0, 1))
+        steps = 0
+
+        while q:
+            for _ in range(len(q)):
+                p, s = q.popleft()
+                if p == target:
+                    return steps
+
+                q.append((p + s, s * 2))
+                if (p + s < target and s < 0) or (p + s > target and s > 0):
+                    q.append((p, -1 if s > 0 else 1))
+            steps += 1
+
+        return -1
 
 
 class Solution:
     def racecar(self, target: int) -> int:
         if target == 0:
             return 0
-        q = Deque()
+        q = deque()
         q.append([0, 1, 0])
 
         while q:
