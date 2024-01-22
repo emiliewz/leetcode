bb42fb1e491ff3759d32cca37ebc43b498978e87
emilie <loveskyship@gmail.com>
2024-01-21 17:23:18 -0800

    935. Knight Dialer

--- DP/935.py
+++ DP/935.py
@@ -4,6 +4,32 @@ from functools import cache
 
 class Solution:
     def knightDialer(self, n: int) -> int:
+        neighbours = {
+            1: [6, 8],
+            2: [7, 9],
+            3: [4, 8],
+            4: [0, 3, 9],
+            5: [],
+            6: [0, 1, 7],
+            7: [2, 6],
+            8: [1, 3],
+            9: [2, 4],
+            0: [4, 6],
+        }
+        MOD = 10**9 + 7
+        a = [1] * 10
+        for _ in range(n - 1):
+            tmp = [0] * 10
+            for i in range(10):
+                for nei in neighbours[i]:
+                    tmp[i] += a[nei] % MOD
+            a = tmp
+
+        return sum(a) % MOD
+
+
+class Solution:
+    def knightDialer(self, n: int) -> int:
         MOD = 10**9 + 7
         neighbors = {
             0: [4, 6],
