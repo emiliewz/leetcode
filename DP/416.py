d64558bd6fd3e76a920a78480bc3bbd42b6eb5c7
emilie <loveskyship@gmail.com>
2024-01-21 18:11:38 -0800

    416. Partition Equal Subset Sum

--- DP/416.py
+++ DP/416.py
@@ -5,6 +5,80 @@ from typing import List
 
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
+        sums = sum(nums)
+        nums.sort()
+        target = sums // 2
+        if sums % 2 or nums[-1] > target:
+            return False
+        if nums[-1] == target:
+            return True
+
+        a = [False] * (target + 1)
+        a[0] = True
+
+        for num in nums:
+            for i in range(target, num - 1, -1):
+                a[i] = a[i] or a[i - num]
+
+        return a[target]
+
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        sums = sum(nums)
+        nums.sort()
+        target = sums // 2
+        if sums % 2 or nums[-1] > target:
+            return False
+        n = len(nums)
+
+        @cache
+        def dp(i, cur):
+            if cur == target:
+                return True
+            if i == n:
+                return False
+
+            for j in range(i, n):
+                if j > i and nums[j] == nums[j - 1]:
+                    continue
+                if nums[j] + cur <= target:
+                    if dp(j + 1, cur + nums[j]):
+                        return True
+            return False
+
+        return dp(0, 0)
+
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
+        sums = sum(nums)
+        nums.sort(reverse=True)
+        target = sums // 2
+        if sums % 2 or nums[0] > target:
+            return False
+        n = len(nums)
+        a = [0] * 2
+
+        def dfs(i):
+            if i == n:
+                return True
+
+            for j in range(2):
+                if a[j] + nums[i] <= target:
+                    a[j] += nums[i]
+                    if dfs(i + 1):
+                        return True
+                    a[j] -= nums[i]
+                    if a[j] == 0:
+                        return False
+            return False
+
+        return dfs(0)
+
+
+class Solution:
+    def canPartition(self, nums: List[int]) -> bool:
         if sum(nums) & 1:
             return False
         target = sum(nums) >> 1
