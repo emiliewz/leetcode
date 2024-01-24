2b23515e3aa89bfa3d3562b31ded36babbeca67f
emilie <loveskyship@gmail.com>
2024-01-24 08:52:56 -0800

    827. Making A Large Island

---
+++ Graph/827.py
@@ -0,0 +1,59 @@
+# 827. Making A Large Island
+from collections import deque
+from typing import List
+
+
+class Solution:
+    def largestIsland(self, grid: List[List[int]]) -> int:
+        def bfs(k):
+            q, move = deque(lands[k]), 0
+            visit = set(lands[k])
+            nei_area = 0
+
+            while q and move < 2:
+                for _ in range(len(q)):
+                    cx, cy = q.popleft()
+                    cur = 0
+                    nei = set()
+
+                    for dx, dy in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
+                        nx, ny = cx + dx, cy + dy
+
+                        if (0 <= nx < n) and (0 <= ny < n):
+                            if (nx, ny) not in visit:
+                                visit.add((nx, ny))
+                                q.append((nx, ny))
+
+                            if grid[nx][ny] not in [0, k]:
+                                nei.add(grid[nx][ny])
+                    cur += sum(lands_size[i] for i in nei)
+                    nei_area = max(cur, nei_area)
+                move += 1
+            return nei_area
+
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= n or j >= n or grid[i][j] in [0, k]:
+                return 0
+            grid[i][j] = k
+            lands.setdefault(k, []).append((i, j))
+            area = 1 + dfs(i + 1, j) + dfs(i - 1, j) + dfs(i, j - 1) + dfs(i, j + 1)
+            return area
+
+        n = len(grid)
+        lands, k = {}, 2
+
+        for i in range(n):
+            for j in range(n):
+                if grid[i][j] == 1:
+                    dfs(i, j)
+                    k += 1
+        max_area = 0
+        if not lands:
+            return 1
+        lands_size = {i: len(lands[i]) for i in lands}
+
+        for i in lands:
+            area = lands_size[i] + 1
+            max_area = max(max_area, area + bfs(i))
+
+        return min(max_area, n**2)
