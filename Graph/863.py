f682673cf81fb664b49962dfd17d42e8a48afe37
emilie <loveskyship@gmail.com>
2024-01-27 15:05:17 -0800

    863. All Nodes Distance K in Binary Tree

---
+++ Graph/863.py
@@ -0,0 +1,46 @@
+# 863. All Nodes Distance K in Binary Tree
+from collections import deque
+from typing import List
+
+
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def distanceK(self, root: TreeNode, target: TreeNode, k: int) -> List[int]:
+        q = deque([root])
+        trees = {}
+
+        while q:
+            cur = q.popleft()
+            if cur.left:
+                trees[cur.left.val] = cur
+                q.append(cur.left)
+            if cur.right:
+                trees[cur.right.val] = cur
+                q.append(cur.right)
+
+        q.append(target)
+        visit = set()
+
+        while k > 0 and q:
+            for _ in range(len(q)):
+                cur = q.popleft()
+                visit.add(cur.val)
+                if cur.left and cur.left.val not in visit:
+                    q.append(cur.left)
+                if cur.right and cur.right.val not in visit:
+                    q.append(cur.right)
+                if cur.val in trees and trees[cur.val].val not in visit:
+                    q.append(trees[cur.val])
+            k -= 1
+        res = []
+        while q:
+            res.append(q.popleft().val)
+
+        return res
