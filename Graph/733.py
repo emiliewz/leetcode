b5cd7ceeaf577583b9d28b0afa5d674246d39125
emilie <loveskyship@gmail.com>
2024-02-03 16:51:06 -0800

    733. Flood Fill

--- Graph/733.py
+++ Graph/733.py
@@ -7,6 +7,25 @@ class Solution:
     def floodFill(
         self, image: List[List[int]], sr: int, sc: int, color: int
     ) -> List[List[int]]:
+        if image[sr][sc] == color:
+            return image
+        m, n = len(image), len(image[0])
+        q = deque([(sr, sc)])
+        start = image[sr][sc]
+        while q:
+            cx, cy = q.popleft()
+            image[cx][cy] = color
+            for dx, dy in [(-1, 0), (1, 0), (0, 1), (0, -1)]:
+                i, j = cx + dx, cy + dy
+                if 0 <= i < m and 0 <= j < n and image[i][j] == start:
+                    q.append((i, j))
+        return image
+
+
+class Solution:
+    def floodFill(
+        self, image: List[List[int]], sr: int, sc: int, color: int
+    ) -> List[List[int]]:
         m, n = len(image), len(image[0])
         cur = image[sr][sc]
         if cur == color:
