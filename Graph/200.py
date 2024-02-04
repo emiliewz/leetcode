5cf5461daa2eda493bac7c4169d0bda0adf0f5ff
emilie <loveskyship@gmail.com>
2024-02-03 16:06:28 -0800

    200. Number of Islands

--- Graph/200.py
+++ Graph/200.py
@@ -4,6 +4,36 @@ from typing import List
 
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        def find(x):
+            if x != p[x]:
+                p[x] = find(p[x])
+            return p[x]
+
+        def union(i, j):
+            p1, p2 = find(i), find(j)
+            p[p1] = p2
+
+        m, n = len(grid), len(grid[0])
+        idx, a = 0, {}
+
+        for i in range(m):
+            for j in range(n):
+                if grid[i][j] == "1":
+                    a[(i, j)] = idx
+                    idx += 1
+        p = list(range(idx))
+
+        for i, j in a:
+            if (i - 1, j) in a:
+                union(a[(i - 1, j)], a[(i, j)])
+            if (i, j - 1) in a:
+                union(a[(i, j)], a[(i, j - 1)])
+
+        return len(set([find(i) for i in range(idx)]))
+
+
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
         res = 0
         m, n = len(grid), len(grid[0])
 
