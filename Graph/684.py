89ac11104b2c123d204a77b2ecd31bf43c576291
emilie <loveskyship@gmail.com>
2024-02-04 18:26:46 -0800

    684. Redundant Connection

--- Graph/684.py
+++ Graph/684.py
@@ -5,7 +5,34 @@ from typing import List
 
 class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
-        n = len(edges)
+        graph = defaultdict(list)
+        for i, j in edges:
+            graph[i].append(j)
+            graph[j].append(i)
+
+        ends = [i for i in graph if len(graph[i]) == 1]
+        if not ends:
+            return edges[-1]
+
+        q = deque(ends)
+        while q:
+            cur = q.popleft()
+
+            for nei in graph[cur]:
+                graph[nei].remove(cur)
+                if len(graph[nei]) == 1:
+                    q.append(nei)
+            graph.pop(cur)
+
+        for i, j in edges[::-1]:
+            if i in graph and j in graph[i]:
+                return [i, j]
+
+        return edges[-1]
+
+
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         graph = defaultdict(set)
 
         for i, j in edges:
