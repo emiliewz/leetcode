ff5ed04f8a8d6fabb1521a742ae73e8fd82f9d6f
emilie <loveskyship@gmail.com>
2024-02-05 09:26:23 -0800

    1334. Find the City With the Smallest Number of Neighbors at a Threshold Distance

--- Graph/1334.py
+++ Graph/1334.py
@@ -8,6 +8,35 @@ class Solution:
     def findTheCity(
         self, n: int, edges: List[List[int]], distanceThreshold: int
     ) -> int:
+        graph = defaultdict(list)
+        for u, v, w in edges:
+            graph[u].append((v, w))
+            graph[v].append((u, w))
+        res = [0] * n
+        for i in range(n):
+            dist = [float("inf")] * n
+            dist[i] = 0
+            h = [(0, i)]
+            while h:
+                cur_d, cur = heappop(h)
+                if cur_d >= distanceThreshold:
+                    break
+                for nei, nei_d in graph[cur]:
+                    new_d = nei_d + cur_d
+                    if new_d < dist[nei]:
+                        dist[nei] = new_d
+                        heappush(h, (new_d, nei))
+            res[i] = sum(j <= distanceThreshold for j in dist) - 1
+        min_connect = min(res)
+        for i in range(n - 1, -1, -1):
+            if res[i] == min_connect:
+                return i
+
+
+class Solution:
+    def findTheCity(
+        self, n: int, edges: List[List[int]], distanceThreshold: int
+    ) -> int:
         def dijkstra(x):
             h = [(0, x)]
             dist = [float("inf")] * n
