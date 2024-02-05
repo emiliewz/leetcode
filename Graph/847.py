9edeb11e50574fc182720d8ea76d8e629198564c
emilie <loveskyship@gmail.com>
2024-02-05 11:18:34 -0800

    847. Shortest Path Visiting All Nodes

--- Graph/847.py
+++ Graph/847.py
@@ -1,6 +1,31 @@
 # 847. Shortest Path Visiting All Nodes
 from collections import deque
+from heapq import heappop, heappush
 from typing import List
+
+
+class Solution:
+    def shortestPathLength(self, graph: List[List[int]]) -> int:
+        if graph == [[]]:
+            return 0
+
+        n = len(graph)
+        complete = (1 << n) - 1
+        h = [(0, 1 << i, i) for i in range(n)]
+        visit = set(h)
+
+        while h:
+            d, m, cur = heappop(h)
+
+            for nei in graph[cur]:
+                mask = m | (1 << nei)
+                if mask == complete:
+                    return d + 1
+                if (mask, nei) not in visit:
+                    visit.add((mask, nei))
+                    heappush(h, (d + 1, mask, nei))
+
+        return -1
 
 
 class Solution:
