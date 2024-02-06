e5557d380915251d4813a542d836993dec0581ab
emilie <loveskyship@gmail.com>
2024-02-05 17:26:26 -0800

    943. Find the Shortest Superstring

--- Graph/943.py
+++ Graph/943.py
@@ -6,6 +6,35 @@ from typing import List
 
 class Solution:
     def shortestSuperstring(self, words: List[str]) -> str:
+        @cache
+        def connect(x, y):
+            f, s = words[x], words[y]
+            i = min(len(f), len(s))
+            while i > 0:
+                if f[-i:] == s[:i]:
+                    return i
+                i -= 1
+            return 0
+
+        n = len(words)
+        complete = (1 << n) - 1
+        a = [[(float("inf"), "")] * n for _ in range(complete + 1)]
+
+        for i in range(n):
+            a[1 << i][i] = (len(words[i]), words[i])
+
+        for mask in range(1 << n):
+            wordInBit = [i for i in range(n) if mask & (1 << i)]
+            for f, s in permutations(wordInBit, 2):
+                new_word = a[mask ^ (1 << s)][f][1] + words[s][connect(f, s) :]
+                if len(new_word) < a[mask][s][0]:
+                    a[mask][s] = (len(new_word), new_word)
+
+        return min(a[-1])[1]
+
+
+class Solution:
+    def shortestSuperstring(self, words: List[str]) -> str:
         n = len(words)
         complete = 1 << n
         a = [[(float("inf"), "")] * n for _ in range(complete)]
