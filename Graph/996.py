7c4ece89de8c92550f5c5fe01a81b2a9e63f34fc
emilie <loveskyship@gmail.com>
2024-02-06 20:26:09 -0800

    996. Number of Squareful Arrays

--- Graph/996.py
+++ Graph/996.py
@@ -6,6 +6,26 @@ from typing import List
 class Solution:
     def numSquarefulPerms(self, nums: List[int]) -> int:
         def dfs(nums, cur):
+            if not nums:
+                nonlocal res
+                res += 1
+                return
+
+            for i in range(len(nums)):
+                if i > 0 and nums[i] == nums[i - 1]:
+                    continue
+                if not cur or isqrt(cur[-1] + nums[i]) ** 2 == cur[-1] + nums[i]:
+                    dfs(nums[:i] + nums[i + 1 :], cur + [nums[i]])
+
+        res = 0
+        nums.sort()
+        dfs(nums, [])
+        return res
+
+
+class Solution:
+    def numSquarefulPerms(self, nums: List[int]) -> int:
+        def dfs(nums, cur):
             if len(cur) == n:
                 nonlocal res
                 res += 1
