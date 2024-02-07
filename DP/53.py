3fca76093e52d821e464f4b416f16965c9f72c66
emilie <loveskyship@gmail.com>
2024-02-07 08:38:14 -0800

    53. Maximum Subarray

--- DP/53.py
+++ DP/53.py
@@ -4,6 +4,17 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
+        max_sum, cur = nums[0], 0
+
+        for n in nums:
+            cur = n + max(cur, 0)
+            max_sum = max(cur, max_sum)
+
+        return max_sum
+
+
+class Solution:
+    def maxSubArray(self, nums: List[int]) -> int:
         max_sub = nums[0]
         cur = 0
 
