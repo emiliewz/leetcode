9dd3755a6c8d685bd20a9b5ae66acedebfc9a676
emilie <loveskyship@gmail.com>
2024-02-06 20:48:15 -0800

    959. Regions Cut By Slashes

--- Graph/959.py
+++ Graph/959.py
@@ -9,6 +9,40 @@ class Solution:
                 p[x] = find(p[x])
             return p[x]
 
+        def union(x, y):
+            p1, p2 = find(x), find(y)
+            if p1 != p2:
+                p[p1] = p2
+
+        n = len(grid)
+        p = list(range(4 * n * n))
+        for i in range(n):
+            for j in range(n):
+                cur = (i * n + j) * 4
+                if grid[i][j] != "/":
+                    union(cur + 0, cur + 3)
+                    union(cur + 1, cur + 2)
+                if grid[i][j] != "\\":
+                    union(cur + 0, cur + 1)
+                    union(cur + 2, cur + 3)
+                if i > 0:
+                    top = ((i - 1) * n + j) * 4
+                    union(cur + 0, top + 2)
+                if j > 0:
+                    left = (i * n + (j - 1)) * 4
+                    union(cur + 1, left + 3)
+
+        res = [find(i) for i in range(4 * n * n)]
+        return len(set(res))
+
+
+class Solution:
+    def regionsBySlashes(self, grid: List[str]) -> int:
+        def find(x):
+            if x != p[x]:
+                p[x] = find(p[x])
+            return p[x]
+
         def union(i, j):
             p1, p2 = find(i), find(j)
             if p1 != p2:
