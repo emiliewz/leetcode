cb375f1123f4066dcdfb477a6b31b9f2483581bf
emilie <loveskyship@gmail.com>
2024-02-07 09:22:21 -0800

    931. Minimum Falling Path Sum

--- DP/931.py
+++ DP/931.py
@@ -5,6 +5,21 @@ from typing import List
 class Solution:
     def minFallingPathSum(self, matrix: List[List[int]]) -> int:
         n = len(matrix)
+        a = matrix[:]
+
+        for i in range(1, n):
+            for j in range(n):
+                a[i][j] += min(
+                    a[i - 1][j - 1] if j > 0 else float("inf"),
+                    a[i - 1][j],
+                    a[i - 1][j + 1] if j < n - 1 else float("inf"),
+                )
+        return min(a[-1])
+
+
+class Solution:
+    def minFallingPathSum(self, matrix: List[List[int]]) -> int:
+        n = len(matrix)
         a = [[float("inf")] + i + [float("inf")] for i in matrix]
 
         for i in range(1, n):
