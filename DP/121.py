f77b07feadc5ddae381905a645bd515dc925f372
emilie <loveskyship@gmail.com>
2024-02-07 08:42:22 -0800

    121. Best Time to Buy and Sell Stock

--- DP/121.py
+++ DP/121.py
@@ -4,6 +4,19 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        max_profit = 0
+        buy = prices[0]
+
+        for p in prices:
+            if p < buy:
+                buy = p
+            max_profit = max(max_profit, p - buy)
+
+        return max_profit
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         buy = prices[0]
         max_pro = 0
 
