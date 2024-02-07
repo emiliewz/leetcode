62846bf8fef55034170a888851d70b2459b14c39
emilie <loveskyship@gmail.com>
2024-02-07 08:17:53 -0800

    303. Range Sum Query - Immutable

--- DP/303.py
+++ DP/303.py
@@ -3,6 +3,18 @@ from typing import List
 
 
 class NumArray:
+
+    def __init__(self, nums: List[int]):
+        n = len(nums)
+        self.sums = [0] * (n + 1)
+        for i in range(1, n + 1):
+            self.sums[i] = self.sums[i - 1] + nums[i - 1]
+
+    def sumRange(self, left: int, right: int) -> int:
+        return self.sums[right + 1] - self.sums[left]
+
+
+class NumArray:
     def __init__(self, nums: List[int]):
         n = len(nums)
         self.sums = [0] * n
