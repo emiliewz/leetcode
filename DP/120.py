384d19fa5412aa45e37b9fe4c89406167090953f
emilie <loveskyship@gmail.com>
2024-02-07 09:04:08 -0800

    120. Triangle

--- DP/120.py
+++ DP/120.py
@@ -5,6 +5,21 @@ from typing import List
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
         n = len(triangle)
+        a = triangle[:]
+
+        for i in range(1, n):
+            for j in range(i + 1):
+                a[i][j] += min(
+                    a[i - 1][j] if j < i else float("inf"),
+                    a[i - 1][j - 1] if j > 0 else float("inf"),
+                )
+
+        return min(a[-1])
+
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        n = len(triangle)
         min_path = triangle[:]
         for i in range(1, n):
             for j in range(i + 1):
