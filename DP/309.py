8a60a5b35c93a36d64db81875f8e4bc79c17ed55
emilie <loveskyship@gmail.com>
2024-02-08 15:50:52 -0800

    309. Best Time to Buy and Sell Stock with Cooldown

--- DP/309.py
+++ DP/309.py
@@ -4,6 +4,34 @@ from typing import List
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        sell = 0
+        cool_down = 0
+        hold = -float("inf")
+
+        for p in prices:
+            hold, sell, cool_down = (
+                max(cool_down - p, hold),
+                hold + p,
+                max(sell, cool_down),
+            )
+
+        return max(sell, cool_down)
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        buy = -float("inf")
+        hold = 0
+        sell = 0
+
+        for p in prices:
+            hold, buy, sell = max(buy, hold, sell), max(hold - p, buy), buy + p
+
+        return max(sell, hold)
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
         rest, sold, hold = 0, 0, -float("inf")
 
         for i in prices:
