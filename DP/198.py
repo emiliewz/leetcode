e90350f252be0d10abc9cfb8c93e571ed8365032
emilie <loveskyship@gmail.com>
2024-02-08 15:33:13 -0800

    198. House Robber

--- DP/198.py
+++ DP/198.py
@@ -7,6 +7,19 @@ class Solution:
         n = len(nums)
         if n == 1:
             return nums[0]
+        f, s = 0, 0
+
+        for i in range(n):
+            f, s = max(nums[i] + s, f), f
+
+        return max(f, s)
+
+
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        n = len(nums)
+        if n == 1:
+            return nums[0]
         f, s = nums[-1], 0
 
         for i in range(n - 2, -1, -1):
