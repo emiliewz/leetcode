815b9a6c9c381f91827b1bd9d4d5a1afadb0f28f
emilie <loveskyship@gmail.com>
2024-02-09 08:25:55 -0800

    96. Unique Binary Search Trees

--- DP/96.py
+++ DP/96.py
@@ -1,6 +1,20 @@
 # 96. Unique Binary Search Trees
 class Solution:
     def numTrees(self, n: int) -> int:
+        if n == 1:
+            return 1
+        if n < 3:
+            return n
+        a = [0] * (n + 1)
+        a[1] = a[0] = 1
+        for i in range(2, n + 1):
+            for j in range(i):
+                a[i] += a[i - j - 1] * a[j]
+        return a[n]
+
+
+class Solution:
+    def numTrees(self, n: int) -> int:
         if n <= 2:
             return n
         dp = [0] * (n + 1)
