4d9bae2c4f86c80802ff9a92d1d79035dc2e742f
emilie <loveskyship@gmail.com>
2024-02-09 08:20:42 -0800

    1048. Longest String Chain

--- DP/1048.py
+++ DP/1048.py
@@ -1,5 +1,45 @@
 # 1048. Longest String Chain
+from functools import cache
 from typing import List
+
+
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        words.sort(key=len)
+        a = {}
+
+        for w in words:
+            a[w] = 1
+            for j in range(len(w)):
+                sub_word = w[:j] + w[j + 1 :]
+                if sub_word in a:
+                    a[w] = max(a[w], a[sub_word] + 1)
+        return max(a.values())
+
+
+class Solution:
+    def longestStrChain(self, words: List[str]) -> int:
+        @cache
+        def canChain(i, j):
+            f, s = words[i], words[j]
+
+            for k in range(length[j]):
+                if f == s[:k] + s[k + 1 :]:
+                    return True
+            return False
+
+        n = len(words)
+        words.sort(key=len)
+        length = [len(i) for i in words]
+        a = [1] * n
+
+        for i in range(1, n):
+            for j in range(i):
+                if length[j] < length[i] - 1 or length[i] == length[j]:
+                    continue
+                if canChain(j, i):
+                    a[i] = max(a[j] + 1, a[i])
+        return max(a)
 
 
 class Solution:
