5d3c035f3a38ef203918cec44f536031aa089906
emilie <loveskyship@gmail.com>
2024-02-08 17:28:53 -0800

    818. Race Car

--- DP/818.py
+++ DP/818.py
@@ -4,6 +4,25 @@ from collections import deque
 
 class Solution:
     def racecar(self, target: int) -> int:
+        q, moves = deque([(0, 1)]), 0
+
+        while q:
+            for _ in range(len(q)):
+                p, s = q.popleft()
+                if p == target:
+                    return moves
+                q.append((p + s, s * 2))
+
+                if (s > 0 and p + s > target) or (s < 0 and p + s < target):
+                    q.append((p, 1 if s < 0 else -1))
+
+            moves += 1
+
+        return -1
+
+
+class Solution:
+    def racecar(self, target: int) -> int:
         q = deque()
         q.append((0, 1))
         res = 0
