e8f54e5d4aab0dc014fc42172206605a7a4efb80
emilie <loveskyship@gmail.com>
2024-02-09 09:56:21 -0800

    89. Gray Code

--- DP/89.py
+++ DP/89.py
@@ -4,6 +4,22 @@ from typing import List
 
 class Solution:
     def grayCode(self, n: int) -> List[int]:
+        return [i ^ (i >> 1) for i in range(1 << n)]
+
+
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
+        res = [0]
+
+        for i in range(n):
+            for j in res[::-1]:
+                res += [j | (1 << i)]
+
+        return res
+
+
+class Solution:
+    def grayCode(self, n: int) -> List[int]:
         res = [0]
         for i in range(n):
             for j in range(len(res) - 1, -1, -1):
