0d9e225488ae9cbb85ec4911e97aed54a601a7c3
emilie <loveskyship@gmail.com>
2024-02-09 09:00:28 -0800

    131. Palindrome Partitioning

--- DP/131.py
+++ DP/131.py
@@ -4,6 +4,23 @@ from typing import List
 
 class Solution:
     def partition(self, s: str) -> List[List[str]]:
+        def dfs(i, cur):
+            if i == n:
+                res.append(cur)
+                return
+
+            for j in range(i + 1, n + 1):
+                if s[i:j] == s[i:j][::-1]:
+                    dfs(j, cur + [s[i:j]])
+
+        n = len(s)
+        res = []
+        dfs(0, [])
+        return res
+
+
+class Solution:
+    def partition(self, s: str) -> List[List[str]]:
         n = len(s)
         if n == 1:
             return [[s]]
