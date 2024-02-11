7b76f8dac96816a8fd827e31547e46bd9e7b9bab
emilie <loveskyship@gmail.com>
2024-02-10 22:12:06 -0800

    1139. Largest 1-Bordered Square

--- DP/1139.py
+++ DP/1139.py
@@ -5,45 +5,16 @@ from typing import List
 class Solution:
     def largest1BorderedSquare(self, grid: List[List[int]]) -> int:
         m, n = len(grid), len(grid[0])
-        max_len = 0
         a = [[(0, 0)] * (n + 1) for _ in range(m + 1)]
+        max_len = 0
 
         for i in range(1, m + 1):
             for j in range(1, n + 1):
-                if grid[i - 1][j - 1] == 1:
-                    a[i][j] = (a[i][j - 1][0] + 1, a[i - 1][j][1] + 1)
-                    if min(a[i][j]) > max_len:
-                        for k in range(min(a[i][j]), max_len, -1):
-                            if a[i][j - k + 1][1] >= k and a[i - k + 1][j][0] >= k:
-                                max_len = k
-                                break
-
-        return max_len**2
-
-
-class Solution:
-    def largest1BorderedSquare(self, grid: List[List[int]]) -> int:
-        m, n = len(grid), len(grid[0])
-        max_len = 0
-
-        # (x, y) in dp, refer to the accumulative width, height
-        dp = [[(0, 0)] * (n + 1) for _ in range(m + 1)]
-
-        for i in range(1, m + 1):
-            if grid[i - 1][0]:
-                max_len = 1
-                dp[i][1] = (1, dp[i - 1][1][1] + 1)
-
-        for i in range(1, n + 1):
-            if grid[0][i - 1]:
-                max_len = 1
-                dp[1][i] = (dp[1][i - 1][0] + 1, 1)
-
-        for i in range(2, m + 1):
-            for j in range(2, n + 1):
                 if grid[i - 1][j - 1]:
-                    dp[i][j] = (dp[i][j - 1][0] + 1, dp[i - 1][j][1] + 1)
-                    for k in range(min(dp[i][j]), max_len, -1):
-                        if min(dp[i - k + 1][j][0], dp[i][j - k + 1][1]) >= k:
-                            max_len = k
+                    a[i][j] = (a[i][j - 1][0] + 1, a[i - 1][j][1] + 1)
+                    cur_max = min(a[i][j])
+                    for k in range(cur_max, max_len, -1):
+                        if a[i - k + 1][j][0] >= k and a[i][j - k + 1][1] >= k:
+                            max_len = max(k, max_len)
+                            break
         return max_len**2
