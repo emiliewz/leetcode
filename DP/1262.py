55204181d5bbd1449d4ad6e171913251cc24ab1a
emilie <loveskyship@gmail.com>
2024-02-11 13:14:54 -0800

    1262. Greatest Sum Divisible by Three

--- DP/1262.py
+++ DP/1262.py
@@ -4,47 +4,8 @@ from typing import List
 
 class Solution:
     def maxSumDivThree(self, nums: List[int]) -> int:
-        a = [0] * 3
-        for n in nums:
-            tmp = a[:]
-            for i in range(3):
-                cur = tmp[i] + n
-                a[cur % 3] = max(a[cur % 3], cur)
+        a = [0, 0, 0]
+        for i in nums:
+            for j in a[:]:
+                a[(i + j) % 3] = max(a[(i + j) % 3], i + j)
         return a[0]
-
-
-class Solution:
-    def maxSumDivThree(self, nums: List[int]) -> int:
-        dp = [0, 0, 0]
-
-        for i in nums:
-            for j in dp[:]:
-                dp[(j + i) % 3] = max(dp[(i + j) % 3], i + j)
-
-        return dp[0]
-
-
-class Solution:
-    def maxSumDivThree(self, nums: List[int]) -> int:
-        dp = [0] * 3
-        for i in nums:
-            a, b, c = dp[0] + i, dp[1] + i, dp[2] + i
-            dp[a % 3] = max(dp[a % 3], a)
-            dp[b % 3] = max(dp[b % 3], b)
-            dp[c % 3] = max(dp[c % 3], c)
-        return dp[0]
-
-
-class Solution:
-    def maxSumDivThree(self, nums: List[int]) -> int:
-        total_sum = sum(nums)
-        sums = [False] * (total_sum + 1)
-        sums[0] = True
-
-        for num in nums:
-            for i in range(total_sum, num - 1, -1):
-                sums[i] = sums[i] or sums[i - num]
-
-        for i in range(total_sum, -1, -1):
-            if sums[i] and i % 3 == 0:
-                return i
