d140e676749ecac8b57200aaa90c27ba01bd99d7
emilie <loveskyship@gmail.com>
2024-02-11 11:29:22 -0800

    322. Coin Change

--- DP/322.py
+++ DP/322.py
@@ -4,59 +4,14 @@ from typing import List
 
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
+        a = [float("inf")] * (amount + 1)
         coins.sort()
-        a = [0] + [float("inf")] * amount
+        a[0] = 0
 
-        for c in coins:
-            if c > amount:
+        for coin in coins:
+            if coin > amount:
                 break
-            for i in range(c, amount + 1):
-                a[i] = min(a[i], a[i - c] + 1)
+            for i in range(coin, amount + 1):
+                a[i] = min(a[i], a[i - coin] + 1)
+
         return a[amount] if a[amount] != float("inf") else -1
-
-
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        coins.sort()
-        dp = [float("inf")] * (amount + 1)
-        dp[0] = 0
-
-        for c in coins:
-            for i in range(c, amount + 1):
-                dp[i] = min(dp[i], dp[i - c] + 1)
-
-        return dp[-1] if dp[-1] != float("inf") else -1
-
-
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0:
-            return 0
-        coins.sort()
-        dp = [float("inf")] * (amount + 1)
-
-        for i in range(amount + 1):
-            for j in coins:
-                if i == j:
-                    dp[i] = 1
-                if i > j:
-                    dp[i] = min(dp[j] + dp[i - j], dp[i])
-
-        return dp[-1] if dp[-1] != float("inf") else -1
-
-
-class Solution:
-    def coinChange(self, coins: List[int], amount: int) -> int:
-        if amount == 0:
-            return 0
-        coins.sort()
-        dp = [float("inf")] * (amount + 1)
-
-        for i in range(amount + 1):
-            for j in range(i):
-                if i in coins:
-                    dp[i] = 1
-                else:
-                    dp[i] = min(dp[j] + dp[i - j], dp[i])
-
-        return dp[-1] if dp[-1] != float("inf") else -1
