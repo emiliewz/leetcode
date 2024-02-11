c114738d59457d3f2b1179007b8eb1fee414da24
emilie <loveskyship@gmail.com>
2024-02-11 14:46:36 -0800

    813. Largest Sum of Averages

--- DP/813.py
+++ DP/813.py
@@ -6,6 +6,34 @@ from typing import List
 class Solution:
     def largestSumOfAverages(self, nums: List[int], k: int) -> float:
         n = len(nums)
+        sums = [0] * (n + 1)
+        for i in range(1, n + 1):
+            sums[i] = sums[i - 1] + nums[i - 1]
+
+        @cache
+        def get_sum(i, j=n):
+            return sums[j] - sums[i]
+
+        @cache
+        def dfs(i, steps):
+            if steps == k - 1:
+                return get_sum(i) / (n - i)
+
+            if n - i <= k - steps:
+                return get_sum(i)
+
+            res = 0
+            for j in range(i + 1, n):
+                cur_avg = get_sum(i, j) / (j - i)
+                res = max(res, cur_avg + dfs(j, steps + 1))
+            return res
+
+        return dfs(0, 0)
+
+
+class Solution:
+    def largestSumOfAverages(self, nums: List[int], k: int) -> float:
+        n = len(nums)
 
         sums = [0] * (n + 1)
         for i in range(1, n + 1):
@@ -18,37 +46,3 @@ class Solution:
                 for p in range(j - 1, i):
                     a[i - 1] = max(a[i - 1], a[p - 1] + (sums[i] - sums[p]) / (i - p))
         return a[-1]
-
-
-class Solution:
-    def largestSumOfAverages(self, nums: List[int], k: int) -> float:
-        @cache
-        def dp(i, steps):
-            if steps == k - 1:
-                return sum(nums[i:]) / len(nums[i:])
-
-            cur_max = 0
-            for j in range(i + 1, len(nums)):
-                cur_avg = sum(nums[i:j]) / (j - i)
-                cur_max = max(cur_max, cur_avg + dp(j, steps + 1))
-
-            return cur_max
-
-        return dp(0, 0)
-
-
-class Solution:
-    def largestSumOfAverages(self, nums: List[int], k: int) -> float:
-        @cache
-        def dfs(i, k):
-            if k == 1:
-                return sum(nums[i:]) / len(nums[i:])
-            maxAvg = 0
-
-            for j in range(i + 1, len(nums)):
-                avg = sum(nums[i:j]) / len(nums[i:j])
-                subarrayAvg = avg + dfs(j, k - 1)
-                maxAvg = max(maxAvg, subarrayAvg)
-            return maxAvg
-
-        return dfs(0, k)
