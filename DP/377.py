d9d18624990b2da7d8b248fb99d86a5edd556cf7
emilie <loveskyship@gmail.com>
2024-02-11 11:59:54 -0800

    377. Combination Sum IV

--- DP/377.py
+++ DP/377.py
@@ -4,50 +4,13 @@ from typing import List
 
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        a = [1] + [0] * target
+        a = [0] * (target + 1)
+        a[0] = 1
         nums.sort()
 
-        for i in range(1, target + 1):
+        for i in range(target + 1):
             for n in nums:
                 if n > i:
                     break
                 a[i] += a[i - n]
-
         return a[target]
-
-
-class Solution:
-    def combinationSum4(self, nums: List[int], target: int) -> int:
-        nums.sort()
-        memo = {}
-
-        def dp(n):
-            if n in memo:
-                return memo[n]
-            if n == 0:
-                return 1
-            if n < nums[0]:
-                return 0
-
-            res = 0
-            for i in nums:
-                if n >= i:
-                    res += dp(n - i)
-
-            memo[n] = res
-            return res
-
-        return dp(target)
-
-
-class Solution:
-    def combinationSum4(self, nums: List[int], target: int) -> int:
-        dp = [0] * (target + 1)
-        dp[0] = 1
-
-        for i in range(1, target + 1):
-            for n in nums:
-                if i >= n:
-                    dp[i] += dp[i - n]
-
-        return dp[-1]
