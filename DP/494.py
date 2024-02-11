d8297e1eceb7085b2834b25ce517804317301684
emilie <loveskyship@gmail.com>
2024-02-11 12:20:47 -0800

    494. Target Sum

--- DP/494.py
+++ DP/494.py
@@ -1,59 +1,22 @@
 # 494. Target Sum
-from functools import cache
 from typing import List
 
 
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        sums = sum(nums)
-        if sums < abs(target) or (sums + target) & 1:
+        total = sum(nums) + target
+        tar = total >> 1
+        if total & 1 or tar < 0:
             return 0
-        new_target = (target + sums) >> 1
-        a = [0] * (new_target + 1)
+
+        a = [0] * (tar + 1)
         a[0] = 1
+        nums.sort()
 
         for n in nums:
-            for i in range(new_target, n - 1, -1):
+            if n > tar:
+                return a[tar]
+            for i in range(tar, n - 1, -1):
                 a[i] += a[i - n]
 
-        return a[new_target]
-
-
-class Solution:
-    def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        @cache
-        def dp(i, cur):
-            if i == len(nums):
-                return int(cur == target)
-            return dp(i + 1, cur - nums[i]) + dp(i + 1, cur + nums[i])
-
-        return dp(0, 0)
-
-
-class Solution:
-    def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        memo = {}
-
-        def dp(i, k):
-            if (i, k) in memo:
-                return memo[(i, k)]
-
-            if i == len(nums):
-                return 1 if k == target else 0
-
-            memo[(i, k)] = dp(i + 1, k + nums[i]) + dp(i + 1, k - nums[i])
-            return memo[(i, k)]
-
-        return dp(0, 0)
-
-
-class Solution:
-    def findTargetSumWays(self, nums: List[int], target: int) -> int:
-        @cache
-        def dp(i, k):
-            if i == len(nums):
-                return 1 if k == target else 0
-
-            return dp(i + 1, k + nums[i]) + dp(i + 1, k - nums[i])
-
-        return dp(0, 0)
+        return a[tar]
