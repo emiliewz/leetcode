568b90ef85643db57a7af9e6c42200acf0aca17d
emilie <loveskyship@gmail.com>
2024-02-12 17:25:34 -0800

    664. Strange Printer

--- DP/664.py
+++ DP/664.py
@@ -5,71 +5,41 @@ from functools import cache
 class Solution:
     def strangePrinter(self, s: str) -> int:
         n = len(s)
-        if n <= 1:
-            return n
-        a = [[float("inf")] * n for _ in range(n)]
+        a = [[n] * n for _ in range(n)]
         for i in range(n):
             a[i][i] = 1
 
         for l in range(2, n + 1):
             for i in range(n - l + 1):
                 j = i + l - 1
-                a[i][j] = 1 + a[i + 1][j]
-                for k in range(i + 1, j + 1):
+                cur = i + 1
+                while cur < n and s[cur] == s[cur - 1]:
+                    cur += 1
+                a[i][j] = a[cur][j] + 1 if cur <= j else 1
+                for k in range(cur, j + 1):
                     if s[k] == s[i]:
-                        a[i][j] = min(
-                            a[i][j], a[i][k - 1] + (a[k + 1][j] if k < j else 0)
-                        )
-
+                        a[i][j] = min(a[i][j], a[cur][k - 1] + a[k][j])
         return a[0][n - 1]
 
 
 class Solution:
     def strangePrinter(self, s: str) -> int:
+        n = len(s)
+
         @cache
-        def dp(l, r):
-            if l > r:
+        def dfs(i, j):
+            if i > j:
                 return 0
 
-            res = 1 + dp(l + 1, r)
-            for i in range(l + 1, r + 1):
-                if s[i] == s[l]:
-                    res = min(res, dp(l, i - 1) + dp(i + 1, r))
+            cur = i + 1
+            while cur < n and s[cur] == s[cur - 1]:
+                cur += 1
+
+            res = dfs(cur, j) + 1
+
+            for h in range(cur, j + 1):
+                if s[h] == s[i]:
+                    res = min(res, dfs(cur, h - 1) + dfs(h, j))
             return res
 
-        return dp(0, len(s) - 1)
-
-
-class Solution:
-    def strangePrinter(self, s: str) -> int:
-        @cache
-        def dp(l, r):
-            if l > r:
-                return 0
-
-            min_count = dp(l + 1, r) + 1
-            for i in range(l + 1, r + 1):
-                if s[i] == s[l]:
-                    min_count = min(min_count, dp(l + 1, i) + dp(i + 1, r))
-
-            return min_count
-
-        return dp(0, len(s) - 1)
-
-
-class Solution:
-    def strangePrinter(self, s: str) -> int:
-        @cache
-        def dp(l, r):
-            if l > r:
-                return 0
-
-            min_count = dp(l, r - 1) + 1
-
-            for i in range(l, r):
-                if s[i] == s[r]:
-                    min_count = min(min_count, dp(l, i) + dp(i + 1, r - 1))
-
-            return min_count
-
-        return dp(0, len(s) - 1)
+        return dfs(0, n - 1)
