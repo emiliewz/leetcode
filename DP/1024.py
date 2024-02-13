d196dcac7d46d1bf45ce514b3ef786fca2a58d65
emilie <loveskyship@gmail.com>
2024-02-12 17:55:24 -0800

    1024. Video Stitching

--- DP/1024.py
+++ DP/1024.py
@@ -4,76 +4,17 @@ from typing import List
 
 class Solution:
     def videoStitching(self, clips: List[List[int]], time: int) -> int:
-        if time == 0:
-            return 0
-
-        clips.sort()
+        clips.sort(key=lambda c: (c[0], -c[1]))
         if clips[0][0] > 0:
             return -1
-
-        a = [float("inf")] * (time + 1)
+        a = [float("inf")] * 101
         a[0] = 0
 
-        for s, e in clips:
-            for i in range(s, min(e, time) + 1):
-                if s <= i <= e:
-                    a[i] = min(a[s] + 1, a[i])
+        for clip in clips:
+            start, end = clip
+            if a[start] == float("inf"):
+                return a[time] if a[time] != float("inf") else -1
+            for i in range(start + 1, end + 1):
+                a[i] = min(a[i], a[start] + 1)
 
-        return a[-1] if a[-1] != float("inf") else -1
-
-
-class Solution:
-    def videoStitching(self, clips: List[List[int]], time: int) -> int:
-        if time == 0:
-            return 0
-
-        clips.sort()
-        if clips[0][0] > 0:
-            return -1
-
-        a = [float("inf")] * (time + 1)
-        a[0] = 0
-
-        for i in range(1, time + 1):
-            for s, e in clips:
-                if s <= i <= e:
-                    a[i] = min(a[s] + 1, a[i])
-
-        return a[-1] if a[-1] != float("inf") else -1
-
-
-class Solution:
-    def videoStitching(self, clips: List[List[int]], time: int) -> int:
-        if time == 0:
-            return 0
-        n = len(clips)
-        clips.sort(key=lambda x: (x[0], -x[1]))
-        if clips[0][0] > 0:
-            return -1
-
-        i = 1
-        stack = [clips[0]]
-        while stack[-1][1] < time and i < n:
-            s, e = clips[i]
-            if s <= stack[-1][1] < e:
-                if len(stack) > 1 and s <= stack[-2][1]:
-                    stack.pop()
-                stack.append(clips[i])
-            i += 1
-
-        if stack[-1][1] >= time:
-            return len(stack)
-        return -1
-
-
-class Solution:
-    def videoStitching(self, clips: List[List[int]], time: int) -> int:
-        clips.sort()
-        dp = [float("inf")] * 101
-        dp[0] = 0
-
-        for s, e in clips:
-            for i in range(s, e + 1):
-                dp[i] = min(dp[s] + 1, dp[i])
-
-        return dp[time] if dp[time] != float("inf") else -1
+        return a[time] if a[time] != float("inf") else -1
