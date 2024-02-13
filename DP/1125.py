712a1c02aac1e0a468ecdf4178d70781113d985d
emilie <loveskyship@gmail.com>
2024-02-13 13:50:47 -0800

    1125. Smallest Sufficient Team

--- DP/1125.py
+++ DP/1125.py
@@ -6,39 +6,20 @@ class Solution:
     def smallestSufficientTeam(
         self, req_skills: List[str], people: List[List[str]]
     ) -> List[int]:
-        a = {0: []}
-        n = len(req_skills)
-        skills = {s: i for i, s in enumerate(req_skills)}
+        skills, a, n = (
+            {j: i for i, j in enumerate(req_skills)},
+            {0: []},
+            len(req_skills),
+        )
+        complete = 1 << n
 
-        for i, person in enumerate(people):
-            prev_skills = 0
-            for s in person:
-                prev_skills |= 1 << skills[s]
+        for person, skill in enumerate(people):
+            cur_mask = 0
+            for s in skill:
+                cur_mask |= 1 << skills[s]
+            for prev_mask, team in list(a.items()):
+                mask = cur_mask | prev_mask
+                if mask not in a or len(a[mask]) > len(team) + 1:
+                    a[mask] = team + [person]
 
-            for s, p in list(a.items()):
-                new_skills = s | prev_skills
-                if not new_skills in a or len(a[new_skills]) > len(p) + 1:
-                    a[new_skills] = p + [i]
-
-        return a[(1 << n) - 1]
-
-
-class Solution:
-    def smallestSufficientTeam(
-        self, req_skills: List[str], people: List[List[str]]
-    ) -> List[int]:
-        n = len(req_skills)
-        dp = {0: []}
-        skill_dict = {skill: i for i, skill in enumerate(req_skills)}
-
-        for i, j in enumerate(people):
-            prev_skills = 0
-            for skill in j:
-                prev_skills |= 1 << skill_dict[skill]
-
-            for skills, team in list(dp.items()):
-                new_skills = prev_skills | skills
-                if not new_skills in dp or len(dp[new_skills]) > len(team) + 1:
-                    dp[new_skills] = team + [i]
-
-        return dp[(1 << n) - 1]
+        return a[complete - 1]
