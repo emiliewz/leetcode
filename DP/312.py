d859ed4fd6cb89ab690559b83e9b656c7ecb37ac
emilie <loveskyship@gmail.com>
2024-02-12 16:34:22 -0800

    312. Burst Balloons

--- DP/312.py
+++ DP/312.py
@@ -14,67 +14,23 @@ class Solution:
                 j = i + l - 1
                 for k in range(i + 1, j):
                     a[i][j] = max(
-                        a[i][j], a[i][k] + a[k][j] + nums[i] * nums[j] * nums[k]
+                        a[i][j], nums[i] * nums[j] * nums[k] + a[i][k] + a[k][j]
                     )
-
         return a[0][n - 1]
 
 
 class Solution:
     def maxCoins(self, nums: List[int]) -> int:
         @cache
-        def dp(l, r):
-            if l > r:
+        def dfs(i, j):
+            if i + 1 == j:
                 return 0
-
-            max_coins = 0
-            for i in range(l, r + 1):
-                coins = nums[l - 1] * nums[r + 1] * nums[i]
-                max_coins = max(max_coins, coins + dp(l, i - 1) + dp(i + 1, r))
-
-            return max_coins
+            res = 0
+            for k in range(i + 1, j):
+                cur = nums[i] * nums[j] * nums[k]
+                res = max(res, cur + dfs(i, k) + dfs(k, j))
+            return res
 
         nums = [1] + nums + [1]
-        return dp(1, len(nums) - 2)
-
-
-class Solution:
-    def maxCoins(self, nums: List[int]) -> int:
-        nums = [1] + nums + [1]
-
-        @cache
-        def dp(l, r):
-            if l > r:
-                return 0
-            if l == r:
-                return nums[l - 1] * nums[l] * nums[l + 1]
-
-            max_coins = 0
-            for i in range(l, r + 1):
-                max_coins = max(
-                    nums[i] * nums[l - 1] * nums[r + 1] + dp(l, i - 1) + dp(i + 1, r),
-                    max_coins,
-                )
-            return max_coins
-
-        return dp(1, len(nums) - 2)
-
-
-class Solution:
-    def maxCoins(self, nums: List[int]) -> int:
-        nums = [1] + nums + [1]
-
-        @cache
-        def dp(l, r):
-            if l > r:
-                return 0
-
-            max_coins = 0
-            for i in range(l, r + 1):
-                coins = (
-                    nums[i] * nums[l - 1] * nums[r + 1] + dp(l, i - 1) + dp(i + 1, r)
-                )
-                max_coins = max(max_coins, coins)
-            return max_coins
-
-        return dp(1, len(nums) - 2)
+        n = len(nums)
+        return dfs(0, n - 1)
