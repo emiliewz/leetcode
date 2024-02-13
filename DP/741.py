3cc7b6c56df51649ab7e2a3e1a7f3c0f2f2a5241
emilie <loveskyship@gmail.com>
2024-02-13 12:09:09 -0800

    741. Cherry Pickup

--- DP/741.py
+++ DP/741.py
@@ -5,6 +5,44 @@ from typing import List
 
 class Solution:
     def cherryPickup(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        if grid[0][0] == -1 or grid[-1][-1] == -1:
+            return 0
+
+        @cache
+        def dfs(x1, y1, x2):
+            y2 = x1 + y1 - x2
+            if (
+                x1 >= n
+                or y1 >= n
+                or grid[x1][y1] == -1
+                or x2 >= n
+                or y2 >= n
+                or grid[x2][y2] == -1
+            ):
+                return -float("inf")
+            if x1 == y1 == n - 1:
+                return grid[x1][y1]
+
+            next_cherry = -float("inf")
+            cur_cherry = grid[x1][y1] + grid[x2][y2]
+
+            if x1 == x2 and y1 == y2:
+                cur_cherry -= grid[x1][y1]
+
+            return cur_cherry + max(
+                next_cherry,
+                dfs(x1 + 1, y1, x2 + 1),
+                dfs(x1, y1 + 1, x2),
+                dfs(x1 + 1, y1, x2),
+                dfs(x1, y1 + 1, x2 + 1),
+            )
+
+        return max(dfs(0, 0, 0), 0)
+
+
+class Solution:
+    def cherryPickup(self, grid: List[List[int]]) -> int:
         if grid[0][0] == -1 or grid[-1][-1] == -1:
             return 0
         n = len(grid)
@@ -25,86 +63,17 @@ class Solution:
 
                     a[r1][c1][r2] = cur_cherry + max(
                         a[r1][c1 + 1][r2] if c1 < n - 1 else -float("inf"),
-                        a[r1][c1 + 1][r2 + 1]
-                        if c1 < n - 1 and r2 < n - 1
-                        else -float("inf"),
+                        (
+                            a[r1][c1 + 1][r2 + 1]
+                            if c1 < n - 1 and r2 < n - 1
+                            else -float("inf")
+                        ),
                         a[r1 + 1][c1][r2] if r1 < n - 1 else -float("inf"),
-                        a[r1 + 1][c1][r2 + 1]
-                        if r1 < n - 1 and r2 < n - 1
-                        else -float("inf"),
+                        (
+                            a[r1 + 1][c1][r2 + 1]
+                            if r1 < n - 1 and r2 < n - 1
+                            else -float("inf")
+                        ),
                     )
 
         return max(0, a[0][0][0])
-
-
-class Solution:
-    def cherryPickup(self, grid: List[List[int]]) -> int:
-        if grid[0][0] == -1 or grid[-1][-1] == -1:
-            return 0
-        n = len(grid)
-
-        @cache
-        def dp(r1, c1, r2):
-            c2 = r1 + c1 - r2
-            if (
-                r1 >= n
-                or c1 >= n
-                or r2 >= n
-                or c2 >= n
-                or grid[r1][c1] == -1
-                or grid[r2][c2] == -1
-            ):
-                return -float("inf")
-
-            if r1 == c1 == n - 1:
-                return grid[-1][-1]
-
-            cur_cherry = grid[r1][c1] if (r1 == r2) else grid[r1][c1] + grid[r2][c2]
-
-            return cur_cherry + max(
-                dp(r1, c1 + 1, r2),
-                dp(r1, c1 + 1, r2 + 1),
-                dp(r1 + 1, c1, r2),
-                dp(r1 + 1, c1, r2 + 1),
-            )
-
-        return max(dp(0, 0, 0), 0)
-
-
-class Solution:
-    def cherryPickup(self, grid: List[List[int]]) -> int:
-        @cache
-        def dp(r1, c1, r2, c2):
-            if (
-                r1 >= n
-                or c1 >= n
-                or r2 >= n
-                or c2 >= n
-                or grid[r1][c1] < 0
-                or grid[r2][c2] < 0
-            ):
-                return -float("inf")
-
-            if r1 == c1 == n - 1:
-                return grid[r1][c1]
-
-            cur_cherry = (
-                grid[r1][c1] if r1 == r2 and c1 == c2 else grid[r1][c1] + grid[r2][c2]
-            )
-
-            next_cherry = -float("inf")
-
-            for i in directions:
-                for j in directions:
-                    next_cherry = max(
-                        next_cherry, dp(r1 + i[0], c1 + i[1], r2 + j[0], c2 + j[1])
-                    )
-
-            return cur_cherry + next_cherry
-
-        if grid[0][0] < 0 or grid[-1][-1] < 0:
-            return 0
-        directions = [(0, 1), (1, 0)]
-        n = len(grid)
-        res = dp(0, 0, 0, 0)
-        return res if res != -float("inf") else 0
