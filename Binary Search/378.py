1817457751012f98ea8a00338de3f27ec64e5c82
emilie <loveskyship@gmail.com>
2024-02-16 19:41:02 -0800

    378. Kth Smallest Element in a Sorted Matrix

---
+++ Binary Search/378.py
@@ -0,0 +1,23 @@
+# 378. Kth Smallest Element in a Sorted Matrix
+from typing import List
+
+
+class Solution:
+    def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
+        n = len(matrix)
+        left, right = matrix[0][0], matrix[n - 1][n - 1]
+
+        while left < right:
+            mid = (left + right) // 2
+            count = 0
+            j = n - 1
+            for i in range(n):
+                while j >= 0 and matrix[i][j] > mid:
+                    j -= 1
+                count += j + 1
+            if count < k:
+                left = mid + 1
+            else:
+                right = mid
+
+        return left
