cdfc0369597dffa46a82841bbcb7a01277ea09e5
emilie <loveskyship@gmail.com>
2024-02-19 17:02:34 -0800

    560. Subarray Sum Equals K

--- HashTable/560.py
+++ HashTable/560.py
@@ -15,3 +15,19 @@ class Solution:
             a[pre_sum] += 1
 
         return res
+
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+        a, total, res = {0: 1}, 0, 0
+
+        for n in nums:
+            total += n
+            if total - k in a:
+                res += a[total - k]
+            if total not in a:
+                a[total] = 1
+            else:
+                a[total] += 1
+
+        return res
