4fc80aa95f1a840ccaa2374870e01575911a07cb
Emilie Zhang <110748021+emiliewz@users.noreply.github.com>
2025-08-23 22:15:40 -0700

    Time: 41 ms (36.64%), Space: 17.6 MB (74.25%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.py
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.py
@@ -4,18 +4,8 @@ class Solution:
         m = len(word2)
         res = ''
 
-        if n < m:
-            for i in range(n):
-                res += word1[i] 
-                res += word2[i] 
-            return res + word2[i+1:]
-        elif n > m:
-            for i in range(m):
-                res += word1[i] 
-                res += word2[i] 
-            return res + word1[i+1:]
-        else:
-            for i in range(m):
-                res += word1[i] 
-                res += word2[i]
-            return res
+        for i in range(min(n,m)):
+            res += word1[i] 
+            res += word2[i] 
+        return res + (word2[i+1:] if n < m else word1[i+1:])
+        
