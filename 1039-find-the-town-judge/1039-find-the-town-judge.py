05ee0187e1c500e90b23c472de088fd3b458910d
Emilie Zhang <110748021+emiliewz@users.noreply.github.com>
2025-08-25 22:20:43 -0700

    Time: 30 ms (9.43%), Space: 20.9 MB (39.66%) - LeetHub

---
+++ 1039-find-the-town-judge/1039-find-the-town-judge.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findJudge(self, n: int, trust: List[List[int]]) -> int:
+        graph = defaultdict(set)
+        candidates = set([i for i in range(1, n+1)])
+        
+        # if len(trust) != n-1:
+        #     return -1
+        
+        for a, b in trust:
+            graph[b].add(a)
+            if a in candidates:
+                candidates.remove(a)
+            
+        if (not candidates) or len(candidates) > 1:
+            return -1
+        
+        judge_candidate = candidates.pop()
+        
+        if len(graph[judge_candidate]) == n-1:
+            return judge_candidate
+            
+        return -1
+            
+            
+            
+            
+            
