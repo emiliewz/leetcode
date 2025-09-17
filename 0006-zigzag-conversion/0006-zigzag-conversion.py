fbd73cc8df02c67899f5b4850063fac5804aa664
Emilie Zhang <110748021+emiliewz@users.noreply.github.com>
2025-09-16 21:18:00 -0700

    Time: 8 ms (77.05%), Space: 17.9 MB (49.82%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,13 @@
+class Solution:
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+        res = [''] * numRows
+        k = 0
+        for i,c in enumerate(s):
+            res[k] += c
+            if (i // (numRows-1)) % 2:
+                k -= 1
+            else:
+                k += 1
+        return ''.join(res)
